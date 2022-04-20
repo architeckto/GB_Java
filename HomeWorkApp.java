7381f99cead51274bd57b479cfe1237ad0d6d127
Architeckto <tserulnikov.p@gmail.com>
2022-04-20 18:30:38 +0700

    Pavel Tserulnikov Homework lesson 1
    Пришлось отключить проверку conditions, т.к. идея тогда не давала запушить в гитхаб

---
+++ HomeWorkApp.java
@@ -0,0 +1,61 @@
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    public static void checkSumSign() {
+
+        int a = -7;
+        int b = 3;
+
+        int result = a + b;
+
+        if (result >= 0){
+            System.out.println("Сумма положительная " + result);
+        }else {
+            System.out.println("Сумма отрицательная " + result);
+        }
+
+    }
+
+    public static void printColor(){
+
+        int value = 101;
+
+        if(value <= 0){
+            System.out.println(value + " Color Red");
+        }if (value > 0 && value <= 100) {
+            System.out.println(value + " Color Yellow");
+        }if (value > 100){
+            System.out.println(value + " Color Green");
+        }
+
+    }
+
+    public static void compareNumbers(){
+
+        int a = 25;
+        int b = 25;
+
+        if(a >= b){
+            System.out.println("a >= b");
+        }else {
+            System.out.println("a < b");
+        }
+
+    }
+
+}
